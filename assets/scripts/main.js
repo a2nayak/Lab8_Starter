d228cfd1936669ddfb507ee47d98e36535c789ca
Anirudh Nayak <anirudhsnayak@gmail.com>
2026-05-24 15:10:13 -0700

    attempt serviceworker fix

--- assets/scripts/main.js
+++ assets/scripts/main.js
@@ -53,7 +53,7 @@ function initializeServiceWorker() {
   // Steps B3-B6 will be *inside* the event listener's function created in B2
   // B3. TODO - Register './sw.js' as a service worker (The MDN article
   //            "Using Service Workers" will help you here) 
-        await navigator.serviceWorker.register('./sw.js', {scope: "/"});
+        await navigator.serviceWorker.register('./sw.js', {scope: "/Lab8_Starter/"});
   // B4. TODO - Once the service worker has been successfully registered, console
   //            log that it was successful.
         console.log('Service Worker registered successfully');
